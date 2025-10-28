45784d80e2b956ebceaee734c0f5f87d2dcc85c3
Alberto Trujillo Gonzalez <altrugon@gmail.com>
2025-10-28 09:22:29 -0700

    Complete Exercise 5A: Rendering embedded components.

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import dynamic from "next/dynamic";
 
 import { loadBlogPost } from "@/helpers/file-helpers";
 import BlogHero from "@/components/BlogHero";
 import CodeSnippet from "@/components/CodeSnippet";
+import Spinner from "@/components/Spinner";
+
+const DivisionGroupsDemo = dynamic(
+  () => import("@/components/DivisionGroupsDemo"),
+  {
+    ssr: false,
+    loading: Spinner,
+  }
+);
 
 import styles from "./postSlug.module.css";
 
@@ -30,7 +40,7 @@ async function BlogPost({ params }) {
       <div className={styles.page}>
         <MDXRemote
           source={blogPost.content}
-          components={{ pre: CodeSnippet }}
+          components={{ pre: CodeSnippet, DivisionGroupsDemo }}
         />
       </div>
     </article>
